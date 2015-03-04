ab01f60032411621fef3b52b6f02487826b850df
Anthony Woods <awoods@raintank.io>
2015-03-04 08:23:29 +0000

    update gitignore to exlude config.json but allow config.js

---
+++ config.js
@@ -0,0 +1,34 @@
+'use strict;'
+
+var config = require('./config/config.json');
+
+function parseEnv(name, value, cnf) {
+	if (name in cnf) {
+		cnf[name] = value;
+		return;
+	}
+	var parts = name.split('_');
+	var pos = 0
+	var found = false
+	while (!found && pos < parts.length) {
+		pos = pos + 1;
+		var group = parts.slice(0,pos).join('_');
+		if (group in cnf){
+			found = true;
+			parseEnv(parts.slice(pos).join('_'), value, cnf[group]);
+		}
+	}
+	if (!found) {
+		console.log("%s not found in config", name);
+	}
+}
+
+// overwrite with Environment variables
+for (var key in process.env) {
+	if (key.indexOf('RAINTANK_') == 0) {
+		var name = key.slice(9);
+		parseEnv(name, process.env[key], config);
+	}
+}
+
+exports.config = config;
