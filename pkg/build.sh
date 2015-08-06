7fecfd2688775d1888dbd24e46b571b044e6dd77
James W. Brinkerhoff <jwb@paravolve.net>
2015-08-06 17:36:49 -0400

    fixed packaging to include go-ping via npmignore, determine package version from npm

--- pkg/build.sh
+++ pkg/build.sh
@@ -2,8 +2,10 @@
 
 # Find the directory we exist within
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
 cd ${DIR}/..
 
 go get -u github.com/tatsushid/go-fastping
 go build -o go-ping
+
 npm install
--- pkg/package.sh
+++ pkg/package.sh
@@ -4,13 +4,12 @@
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cd ${DIR}
 
-NAME=raintank-collector
-#VERSION="$(../${NAME} -v | cut -f3 -d' ')"
-#BUILD="${DIR}/${NAME}-${VERSION}"
-#ARCH="$(uname -m)"
-#PACKAGE_NAME="${DIR}/artifacts/${NAME}-VERSION-ITERATION_ARCH.deb"
+: ${NAME:="raintank-collector"}
+: ${VERSION:=$(npm list | head -1 | awk '{print $1}' | cut -f2 -d@)}
 
-cd ${DIR}/artifacts
+mkdir -p ${DIR}/artifacts
+rm -f ${DIR}/artifacts/*
 
-fpm -s npm -t deb \
-  --iteration `date +%s` -d nodejs -d nodejs-legacy -d nodejs-dev -d npm ${DIR}/../
+fpm -s npm -t deb --iteration `date +%s` \
+  -d nodejs -d nodejs-legacy -d nodejs-dev -d npm -d fping \
+  -p ${DIR}/artifacts/NAME-VERSION_ARCH.deb ../
