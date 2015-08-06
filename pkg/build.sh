aaef7d0cefcbb765d4dea6b19c9bd11143a356f8
James W. Brinkerhoff <jwb@paravolve.net>
2015-08-06 11:08:29 -0400

    add DIR handling to build

--- pkg/build.sh
+++ pkg/build.sh
@@ -1,4 +1,9 @@
-#!/bin/sh
+#!/bin/bash
+
+# Find the directory we exist within
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+cd ${DIR}/..
+
 go get -u github.com/tatsushid/go-fastping
 go build -o go-ping
 npm install
