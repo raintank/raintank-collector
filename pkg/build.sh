0151b50ba5c0a42c73ca60613c45ba10af21fe69
James W. Brinkerhoff <jwb@paravolve.net>
2015-08-06 18:56:54 -0400

    cleaned up build proecess.   Generate clean deb that installs to /opt/raintank-collector

--- pkg/build.sh
+++ pkg/build.sh
@@ -3,9 +3,23 @@
 # Find the directory we exist within
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-cd ${DIR}/..
+: ${NAME:="raintank-collector"}
+: ${VERSION:=$(npm list | head -1 | awk '{print $1}' | cut -f2 -d@)}
+: ${BUILD_DIR:="${DIR}/build"}
+COLLECTOR_DIR="/opt/${NAME}"
+COLLECTOR_BUILD_DIR=${BUILD_DIR}${COLLECTOR_DIR}
 
-go get -u github.com/tatsushid/go-fastping
-go build -o go-ping
+# remove any existing BUILD_DIR
+rm -rf ${BUILD_DIR}
 
+mkdir -p ${BUILD_DIR}
+mkdir -p ${COLLECTOR_BUILD_DIR}
+
+cp -fr *.go *.js package.json checks ${COLLECTOR_BUILD_DIR}/
+cp -fR ${DIR}/config/ubuntu/trusty/* ${BUILD_DIR}/
+
+cd ${COLLECTOR_BUILD_DIR}
 npm install
+go get -u github.com/tatsushid/go-fastping
+go build -o go-ping && rm -f go-ping.go
+
