8a0912ee529ffe5ce906261740c88e22df6c91e7
James W. Brinkerhoff <jwb@paravolve.net>
2015-08-06 18:56:54 -0400

    cleaned up build proecess.   Generate clean deb that installs to /opt/raintank-collector

--- pkg/package.sh
+++ pkg/package.sh
@@ -2,14 +2,20 @@
 
 # Find the directory we exist within
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-cd ${DIR}
 
 : ${NAME:="raintank-collector"}
 : ${VERSION:=$(npm list | head -1 | awk '{print $1}' | cut -f2 -d@)}
+: ${BUILD_DIR:="${DIR}/build"}
+COLLECTOR_DIR="/opt/${NAME}"
+COLLECTOR_BUILD_DIR=${BUILD_DIR}${COLLECTOR_DIR}
+
+cd ${DIR}
 
 mkdir -p ${DIR}/artifacts
 rm -f ${DIR}/artifacts/*
 
-fpm -s npm -t deb --iteration `date +%s` \
+fpm -s dir -t deb --iteration `date +%s` \
+  -n ${NAME} -v ${VERSION} \
   -d nodejs -d nodejs-legacy -d nodejs-dev -d npm -d fping \
-  -p ${DIR}/artifacts/NAME-VERSION-ITERATION_ARCH.deb ../
+  -p ${DIR}/artifacts/NAME-VERSION-ITERATION_ARCH.deb \
+  --config-files etc -C ${BUILD_DIR} opt etc
