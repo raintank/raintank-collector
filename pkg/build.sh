8995dbc293b057959826380f8e3bdf0702b004ac
ctdk <jbingham@gmail.com>
2015-08-10 09:23:59 -0700

    don't gripe about raintank-probe's checkout

--- pkg/build.sh
+++ pkg/build.sh
@@ -5,7 +5,7 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cd ${DIR}/..
 rm -rf ~/.go_workspace/*
 ls -la ~/.go_workspace/
-go get -u github.com/raintank/raintank-probe
+go get -u -f github.com/raintank/raintank-probe
 ls -la ~/.go_workspace/
 ls -la ~/.go_workspace/src/
 cp $(which raintank-probe) .
