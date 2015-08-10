6bbb200b2349b4571a5c2ec37518d4591043e6a7
woodsaj <anthony@monkey.id.au>
2015-08-10 23:14:04 +0800

    add debuging to build script

--- pkg/build.sh
+++ pkg/build.sh
@@ -3,10 +3,10 @@
 # Find the directory we exist within
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cd ${DIR}/..
-
+rm -rf ~/.go
 go get -u github.com/raintank/raintank-probe
 echo $PATH
 which raintank-probe
-ls -l ~/.go_workspace/bin/
+ls -la ~/.go_workspace/
 cp ~/.go_workspace/bin/raintank-probe .
 npm install
