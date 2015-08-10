95439b9237c5c7ce44f585c9ca625ebcff77a61e
woodsaj <anthony@monkey.id.au>
2015-08-10 23:18:53 +0800

    remove existing go src before fetching raintank-probe

--- pkg/build.sh
+++ pkg/build.sh
@@ -3,7 +3,7 @@
 # Find the directory we exist within
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cd ${DIR}/..
-rm -rf ~/.go
+rm -rf ~/.go_workspace/*
 go get -u github.com/raintank/raintank-probe
 echo $PATH
 which raintank-probe
