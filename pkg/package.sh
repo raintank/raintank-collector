0c6f163fb1e8ca8e23b21ea493e856eb7eb33c44
ctdk <jbingham@gmail.com>
2015-08-14 12:21:17 -0700

    Tag the repo with the version number + iteration of the package being built

--- pkg/package.sh
+++ pkg/package.sh
@@ -9,13 +9,17 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 : ${BUILD_DIR:="${DIR}/build"}
 COLLECTOR_DIR="/usr/local/lib/node_modules/${NAME}"
 COLLECTOR_BUILD_DIR=${BUILD_DIR}${COLLECTOR_DIR}
+ITERATION=`date +%s`
+TAG="pkg-${VERSION}-${ITERATION}"
 
 cd ${DIR}
+git tag $TAG
+git push --tags
 
 mkdir -p ${DIR}/artifacts
 rm -f ${DIR}/artifacts/*
 
-fpm -s dir -t deb --iteration `date +%s` \
+fpm -s dir -t deb --iteration $ITERATION \
   -n ${PKG_NAME} -v ${VERSION} \
   --description "Raintank Remote Collector Agent" \
   -d nodejs -d nodejs-legacy -d nodejs-dev -d npm -d fping \
