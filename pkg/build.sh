485eca3513181c36a2117ec42968459c3dca8581
ctdk <jbingham@gmail.com>
2015-11-24 13:56:14 -0800

    jessie packaging, stowing a way for later

--- pkg/build.sh
+++ pkg/build.sh
@@ -6,11 +6,14 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 : ${NAME:="raintank-collector"}
 : ${VERSION:=$(npm list | head -1 | awk '{print $1}' | cut -f2 -d@)}
 : ${BUILD_DIR:="${DIR}/build"}
+: ${BUILD_DIR_SYSTEMD:="${DIR}/build-systemd"}
 COLLECTOR_DIR="/usr/local/lib/node_modules/${NAME}"
 COLLECTOR_BUILD_DIR=${BUILD_DIR}${COLLECTOR_DIR}
+COLLECTOR_BUILD_DIR_SYSTEMD=${BUILD_DIR_SYSTEMD}${COLLECTOR_DIR}
 
 # remove any existing BUILD_DIR
 rm -rf ${BUILD_DIR}
+rm -rf ${BUILD_DIR_SYSTEMD}
 
 mkdir -p ${BUILD_DIR}
 mkdir -p ${COLLECTOR_BUILD_DIR}
@@ -24,3 +27,9 @@ npm install
 go get -u -f github.com/raintank/raintank-probe
 cp $(which raintank-probe) .
 
+# and systemd
+mkdir -p $BUILD_DIR_SYSTEMD
+mkdir -p $COLLECTOR_BUILD_DIR_SYSTEMD
+
+cp -fr ${COLLECTOR_BUILD_DIR}/* $COLLECTOR_BUILD_DIR_SYSTEMD
+cp -fR ${DIR}/config/debian/jessie/* $BUILD_DIR_SYSTEMD
--- pkg/package.sh
+++ pkg/package.sh
@@ -7,9 +7,13 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 : ${PKG_NAME:="node-${NAME}"}
 : ${VERSION:=$(npm list | head -1 | awk '{print $1}' | cut -f2 -d@)}
 : ${BUILD_DIR:="${DIR}/build"}
+: ${BUILD_DIR_SYSTEMD:="${DIR}/build-systemd"}
 COLLECTOR_DIR="/usr/local/lib/node_modules/${NAME}"
 COLLECTOR_BUILD_DIR=${BUILD_DIR}${COLLECTOR_DIR}
+COLLECTOR_BUILD_DIR_SYSTEMD=${BUILD_DIR_SYSTEMD}${COLLECTOR_DIR}
+
 ITERATION=`date +%s`
+echo $ITERATION > $DIR/iteration
 TAG="pkg-${VERSION}-${ITERATION}"
 
 cd ${DIR}
@@ -24,3 +28,10 @@ fpm -s dir -t deb --iteration $ITERATION \
   -d nodejs -d nodejs-legacy -d nodejs-dev -d npm -d fping \
   -p ${DIR}/artifacts/NAME-VERSION-ITERATION_ARCH.deb \
   --config-files etc -C ${BUILD_DIR} usr etc
+
+fpm -s dir -t deb --iteration ${ITERATION}jessie \
+  -n ${PKG_NAME} -v ${VERSION} \
+  --description "Raintank Remote Collector Agent" \
+  -d nodejs -d nodejs-legacy -d nodejs-dev -d npm -d fping \
+  -p ${DIR}/artifacts/NAME-VERSION-ITERATION_ARCH.deb \
+  --config-files etc -C ${BUILD_DIR} usr etc lib
