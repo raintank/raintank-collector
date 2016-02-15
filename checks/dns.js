9f00daaf99d704e1eb7a2e1b8077dc5e58fd56dd
Jan Garaj <jan.garaj@gmail.com>
2016-02-15 20:24:13 +0000

    Custom log level

--- checks/dns.js
+++ checks/dns.js
@@ -5,6 +5,7 @@ var util = require('util');
 http.globalAgent.maxSockets = 1000;
 var log4js = require('log4js');
 var logger = log4js.getLogger('PID:'+process.pid);
+logger.setLevel(config.logLevel);
 
 exports.execute = function(payload, service, config, timestamp, callback) {
     var profile = {
